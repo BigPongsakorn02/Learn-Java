64c1542160f90160b3a0b99437de72f7a35eb1b9
BigZ <pongsakornjansanit@hotmail.com>
2025-03-11 17:53:20 +0700

    W5 debug

--- Week05/MyFirstJUnitJupiterTests.java
+++ Week05/MyFirstJUnitJupiterTests.java
@@ -1,16 +1,16 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import example.util.Calculator;
+// import example.util.Calculator;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-class MyFirstJUnitJupiterTests {
+// class MyFirstJUnitJupiterTests {
 
-    private final Calculator calculator = new Calculator();
+//     private final Calculator calculator = new Calculator();
 
-    @Test
-    void addition() {
-        assertEquals(2, calculator.add(1, 1));
-    }
+//     @Test
+//     void addition() {
+//         assertEquals(2, calculator.add(1, 1));
+//     }
 
-} 
+// } 
