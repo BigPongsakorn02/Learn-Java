f976c306bec6548d4d0aaca2555bc21185e9f3b7
BigZ <pongsakornjansanit@hotmail.com>
2025-03-12 10:00:43 +0700

    Week06

---
+++ W6_TicTacToe/app/src/test/java/ticTacToeTest/AppTest.java
@@ -0,0 +1,9 @@
+package ticTacToeTest;
+
+
+// class AppTest {
+//     @Test void appHasAGreeting() {
+//         App classUnderTest = new App();
+//         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+//     }
+// }
